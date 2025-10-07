a8bf51e24d0489e757f8a95362cf3f8dd90dc481
Charles-lang7T7 <tenador.charliejay@mdci.edu.ph>
2025-10-07 21:48:46 +0800

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
-import loadProducts from "/loadProducts.js";
+import loadProducts from "./loadProducts.js";
 
 const cart = () => {
   let listCartHTML = document.querySelector(".listCart");
@@ -153,3 +153,4 @@ const cart = () => {
 };
 
 export default cart;
+
