27eef798e86f6e919497135b4e44d0f6ada806cf
Charles-lang7T7 <tenador.charliejay@mdci.edu.ph>
2025-10-07 23:13:03 +0800

    Update loadProducts.js

--- loadProducts.js
+++ loadProducts.js
@@ -15,43 +15,43 @@ const loadProducts = () => {
         id: 1,
         name: "Ducati Corse D-air® K3",
         price: 699,
-        image: "/39-Dair-K3-b-1920x1536.png",
+        image: "/SPEEDMOTO/39-Dair-K3-b-1920x1536.png",
       },
       {
         id: 2,
         name: "Ducati Corse D-air® C3",
         price: 549,
-        image: "/40-Dair-C3.-b-1920x1536.png",
+        image: "/SPEEDMOTO/40-Dair-C3.-b-1920x1536.png",
       },
       {
         id: 3,
         name: "Ducati Corse D-air® C3 Lady",
         price: 549,
-        image: "/41-Dair-C3-lady-b-1920x1536.png",
+        image: "/SPEEDMOTO/41-Dair-C3-lady-b-1920x1536.png",
       },
       {
         id: 4,
         name: "Ducati Corse C6",
         price: 800,
-        image: "/2023-C6-Corse-b-1920x1536.png",
+        image: "/SPEEDMOTO/2023-C6-Corse-b-1920x1536.png",
       },
       {
         id: 5,
         name: "Ducati Corse C5 Lady",
         price: 499,
-        image: "/2023-C52PC-Lady-b-1920x1536.png",
+        image: "/SPEEDMOTO/2023-C52PC-Lady-b-1920x1536.png",
       },
       {
         id: 6,
         name: "Ducati Corse C5",
         price: 499,
-        image: "/2023-C52PC-Man-b-1920x1536.png",
+        image: "/SPEEDMOTO/2023-C52PC-Man-b-1920x1536.png",
       },
       {
         id: 7,
         name: "Ducati Corse K3",
         price: 999,
-        image: "/Ducati-Corse-K3-b-1920x1536.png",
+        image: "/SPEEDMOTO/Ducati-Corse-K3-b-1920x1536.png",
       },
     ];
 
@@ -67,3 +67,4 @@ const loadProducts = () => {
 };
 
 export default loadProducts;
+
